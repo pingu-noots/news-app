193f44ecc531366f9419ab21d9066b6d56dca6f2
pingue-noots <high.bridge031900@gmail.com>
2022-01-03 02:17:47 +0900

    2021-1-2

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
+import ListItem from './components/ListItem'
+import dummyArticles from './dummies/articles.json'
+import Constants from 'expo-constants'
+import axios from 'axios'
+//import { newsApiKey } from 'react-native-dotenv'
+import { newsApiKey } from '@env'
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${newsApiKey}`
+const BITCOIN = `https://newsapi.org/v2/everything?q=tesla&from=2021-12-02&sortBy=publishedAt&apiKey=${newsApiKey}`
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [articles, setArticles] = useState([])
+    useEffect(() => {
+        fetchArticles()
+    }, [])
+
+    const fetchArticles = async () => {
+        try {
+            const response = await axios.get(URL)
+            setArticles(response.data.articles)
+            console.log(response)
+        } catch {
+            console.log(error)
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={articles}
+                renderItem={({ item }) => (
+                    <ListItem
+                        imageUrl={item.urlToImage}
+                        title={item.title}
+                        author={item.author}
+                    />
+                )}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    itemContainer: {
+        height: 100,
+        width: '100%',
+        borderColor: 'gray',
+        borderWidth: 1,
+        flexDirection: 'row',
+    },
+    leftContainer: {
+        width: 100,
+    },
+    rightContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 10,
+        justifyContent: 'space-between',
+    },
+    text: {
+        fontSize: 16,
+    },
+    subText: {
+        fontSize: 12,
+        color: 'gray',
+    },
+})
